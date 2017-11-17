0661363327ac3a2c99d80690f9414ef808839014
Nathan Daniel <nathanpdaniel01@gmail.com>
2017-11-17 04:24:57 -0500

    Adds initial chart

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
-import { INationalEstimateResponse } from '../../model/index';
+import Grid from 'material-ui/Grid';
+import { VictoryChart, VictoryLine, VictoryLegend, VictoryGroup } from 'victory';
+import { INationalEstimateResponse, INationalEstimate } from '../../model/index';
 import { DataAction } from '../../actions/DataActions';
 
 interface Props {
@@ -8,15 +10,46 @@ interface Props {
 }
 
 class Main extends React.Component<Props> {
+  private CAPITA: number = 100000;
   constructor(props: Props) {
     super(props);
   }
   componentDidMount() {
     this.props.getNationalEstimates();
   }
+
   public render() {
-    console.log(this.props.data);
-    return (<div>HELLO</div>);
+    return (
+      <div>
+        <Grid container={true} spacing={24}>
+          <Grid item={true} style={{ margin: '0 auto' }}>
+            <VictoryChart width={1080} height={607} padding={{ left: 100, right: 100, bottom: 100, top: 100 }}>
+              <VictoryLegend />
+              <VictoryGroup>
+                <VictoryLine
+                  data={this.props.data.results}
+                  x={(d: INationalEstimate) => d.year}
+                  y={this.homicideRate}
+                />
+                <VictoryLine
+                  data={this.props.data.results}
+                  x={(d: INationalEstimate) => d.year}
+                  y={this.violentCrimeRate}
+                />
+              </VictoryGroup>
+            </VictoryChart>
+          </Grid>
+        </Grid>
+      </div>
+    );
+  }
+
+  private homicideRate = (d: INationalEstimate): number => {
+    return (d.homicide / d.population) * this.CAPITA;
+  }
+
+  private violentCrimeRate = (d: INationalEstimate): number => {
+    return (d.violent_crime / d.population) * this.CAPITA;
   }
 }
 
